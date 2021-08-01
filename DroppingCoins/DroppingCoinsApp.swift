eb4c256fe5e3ad8cba5dfa7ba73e4492b7a8b567
isaac <baa44422@gmail.com>
2021-08-01 20:22:58 +0800

    Initial Commit

---
+++ DroppingCoins/DroppingCoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DroppingCoinsApp.swift
+//  DroppingCoins
+//
+//  Created by Isaac Lee Jing Zhi on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct DroppingCoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
